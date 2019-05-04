4c287b7dbefaeaef19969a434a89907e3a01dacf
Eric Chernoff <eric@mail.weaverqueue.com>
2019-05-04 17:19:13 -0400

    Add code

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Backblaze::B2V2Client - It's new $module
+
+# SYNOPSIS
+
+    use Backblaze::B2V2Client;
+
+# DESCRIPTION
+
+Backblaze::B2V2Client is ...
+
+# LICENSE
+
+Copyright (C) ericschernoff.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+ericschernoff <>
---
+++ lib/Backblaze/B2V2Client.pm
@@ -0,0 +1,39 @@
+package Backblaze::B2V2Client;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Backblaze::B2V2Client - It's new $module
+
+=head1 SYNOPSIS
+
+    use Backblaze::B2V2Client;
+
+=head1 DESCRIPTION
+
+Backblaze::B2V2Client is ...
+
+=head1 LICENSE
+
+Copyright (C) ericschernoff.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+ericschernoff E<lt>E<gt>
+
+=cut
+
